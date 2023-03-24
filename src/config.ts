bd860b24c8134a3633c55b1c5bca5eec58dbd0f8
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2023-03-24 15:16:32 +0900

    カッコ除去

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config = {
     author: 'Nectarition'
   },
   event: {
-    name: '小樽潮風高校文化祭「しおばな祭」',
+    name: '小樽潮風高校文化祭 しおばな祭',
     date: '2023-11-04',
     place: '池袋・みらい館大明',
     content: 'TOKYO6キャラクターオンリーイベント',
