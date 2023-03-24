9a38868a19248c0ed382f2305ed9f076c9cf974f
SAIGUSA Tomotada <31317056+10mocy@users.noreply.github.com>
2023-03-24 14:53:46 +0900

    create context

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+const config = {
+  siteUrl: 'https://shiokazehs.jp',
+  copyright: {
+    year: 2022,
+    author: 'Nectarition'
+  },
+  event: {
+    name: '小樽潮風高校文化祭「しおばな祭」',
+    date: '2023-11-04',
+    place: '池袋・みらい館大明',
+    content: 'TOKYO6キャラクターオンリーイベント',
+    twitterId: 'nct_kohatabe'
+  }
+}
+
+export default config
